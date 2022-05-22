e3fbaaabbfdd6e5d02798695f5a7ffda9e1d3e0e
Yuliia Kotsiurba <ulia44@ukr.net>
2022-05-22 17:21:03 +0100

    make hamburger menu and smooth scroll

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,46 @@
+"use strict";
+
 import '../styles/index.scss';
-console.log('webpack starterkit');
 
 
-function start(name) {
-    console.log(name);
+//hamburger menu
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('lock');
+		iconMenu.classList.toggle('active');
+		menuBody.classList.toggle('active');
+	});
 }
 
-start('хрюндель')
+//smooth scroll
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if(menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+			if(iconMenu.classList.contains('active')) {
+				document.body.classList.remove('lock');
+				iconMenu.classList.remove('active');
+				menuBody.classList.remove('active');
+			}
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
+
